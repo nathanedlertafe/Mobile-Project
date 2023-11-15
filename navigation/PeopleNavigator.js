c169557913f3a3931bb5bb9b09d7e26e88a15f89
Nathan <nathan.edler@studytafensw.edu.au>
2023-11-15 20:23:39 +1100

    Initiated

---
+++ navigation/PeopleNavigator.js
@@ -0,0 +1,28 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+
+// Import navigation and screens
+import ViewPeopleScreen from '../screens/ViewPeopleScreen';
+
+// Import styling and components
+import Styles from "../styles/MainStyle";
+
+const Stack = createStackNavigator();
+
+export default function PeopleNavigator() {
+  return (
+    <Stack.Navigator 
+      initialRouteName="ViewPeople"
+      screenOptions={{
+        headerShown: true,
+        headerBackTitle: "Back",
+        headerStyle: Styles.headerBar,
+        headerTitleStyle: Styles.headerBarTitle,
+      }}>
+      <Stack.Screen
+        name="ViewPeople"
+        component={ViewPeopleScreen}
+        options={{ title: 'View All People' }} />
+    </Stack.Navigator>
+  );
+}
